160ef9579c8a5301731ccd03398c6e2766b5765a
mathiashole <mangino@fcien.edu.uy>
2025-08-26 22:56:08 -0300

    Update data frame

--- seqinr_function.R
+++ seqinr_function.R
@@ -8,10 +8,10 @@ length_seq <- function(fasta_file) {
   
   # creo una tabla id de cada secuencia y su largo, guardandolo en dos columnas id y length
   results <- data.frame(
-    id = names(seqs),
-    length = sapply(seqs, length) # 
+    id = names(seqs), # obtiene cada uno de los id de las lista de secuencias
+    length = unlist(apply(seqs, length)) #apply permite aplicar una funcion a una lista, devuelve una lista que la paso a un vector
   )
   
-  return(results)
+  return(results) # retorno el resultado en un data frame
 }
 
