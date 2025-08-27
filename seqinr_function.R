ff2c06d828efa412b0f886ea9079fd31c815366f
mathiashole <mangino@fcien.edu.uy>
2025-08-26 22:30:30 -0300

    Update just only length

--- seqinr_function.R
+++ seqinr_function.R
@@ -4,15 +4,12 @@ library(seqinr)
 # creao una funcion con un solo argumento. Se le debe pasar un archivo fasta como argumento
 length_seq <- function(fasta_file) {
   # lee archivo multifasta
-  seqs <- read.fasta(fasta_file, as.string = TRUE, forceDNAtolower = FALSE)
+  seqs <- read.fasta(fasta_file)
   
   # Crear tabla con resultados
   results <- data.frame(
     Sequence_ID = names(seqs),
     Length = sapply(seqs, length),
-    GC_Content = sapply(seqs, function(x) {
-      round(GC(x) * 100, 2)  # Porcentaje de GC con 2 decimales
-    }),
     stringsAsFactors = FALSE
   )
   
